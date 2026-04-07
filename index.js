9da367baa31eb1a4d7e5b28ea022e7d794bc0854
cory <cory.martel@student.moringaschool.com>
2026-04-07 15:37:27 +0300

    modified the js file

--- index.js
+++ index.js
@@ -45,3 +45,30 @@ const bookStore = {
 
 // Write your code here!
 
+const bookStoreTitle = document.getElementById('header');
+
+bookStoreTitle.textContent = bookStore.name;
+
+const bookList = document.getElementById('book-list');
+bookStore.books.forEach(book => {
+    console.log("Image source being used: ", book.imageUrl);
+  const bookContainer = document.createElement('li');
+
+  const bookTitle = document.createElement('h3');
+
+  const bookAuthor = document.createElement('p');
+
+  const bookImage = document.createElement('img');
+
+
+bookTitle.textContent = book.title;
+
+bookAuthor.textContent = book.author;
+
+bookImage.src = book.imageUrl;
+
+bookContainer.append(bookTitle, bookAuthor, bookImage);
+
+bookList.append(bookContainer);
+});
+console.log(bookStore.books);
